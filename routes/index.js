8a2334cd9c54e0e4953938cc9f6119e9013f0baa
humphr33y <75574561+humphr33y@users.noreply.github.com>
2020-12-06 21:35:30 +0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const user = require('../models/user')
+const user = require('../models/User')
 
 router.get('/', async (req, res) => {
     const users = await user.find({})
